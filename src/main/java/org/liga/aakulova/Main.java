25b7a4d1fb13a6ddf344f969cb45737c39ecaa24
annakulova <aakulovaa03@gmail.com>
2026-09-12 21:22:46 +0300

    Реализовала сервис, отвечающий за логику формирования календаря

--- src/main/java/org/liga/aakulova/Main.java
+++ src/main/java/org/liga/aakulova/Main.java
@@ -1,35 +1,35 @@
 package org.liga.aakulova;
 
 
-import org.liga.aakulova.domain.Calendar;
-import org.liga.aakulova.domain.Day;
-import org.liga.aakulova.domain.Month;
-import org.liga.aakulova.domain.MonthOfCalendar;
+import org.liga.aakulova.domain.*;
+import org.liga.aakulova.service.CalendarService;
+import org.liga.aakulova.service.CalendarServiceImpl;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        CalendarInterface calendarInterface = new CalendarInterface() {
+            @Override
+            public int getDaysCount(int year, Month month) {
+                return 30;
+            }
+
+            @Override
+            public Day getFirstDayOfWeek(int year, Month month) {
+                return Day.MONDAY;
+            }
+        };
+
+        CalendarService calendarService = new CalendarServiceImpl(calendarInterface);
+
         System.out.println("Введите год: ");
 
-        Scanner scanner = new Scanner(System.in);
         int year = scanner.nextInt();
-
-        List<MonthOfCalendar> months = new ArrayList<>();
-
-        for (Month month : Month.values()){
-            months.add(
-                    new MonthOfCalendar(
-                            Day.MONDAY,
-                            month,
-                            30
-                    )
-            );
-        }
-
-        Calendar calendar = new Calendar(year, months);
+        
+        Calendar calendar = calendarService.createCalendar(year);
 
         System.out.println("Введенный год: " + calendar.getYear());
         System.out.println("Количество месяцев: " + calendar.getMonths().size());
---
+++ src/main/java/org/liga/aakulova/service/CalendarService.java
@@ -0,0 +1,7 @@
+package org.liga.aakulova.service;
+
+import org.liga.aakulova.domain.Calendar;
+
+public interface CalendarService {
+    Calendar createCalendar(int year);
+}
